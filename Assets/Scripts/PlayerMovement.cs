092dff3cd7097080c51eac44a7a5a694c40ee521
mguellaff <mguellaff@GCFRPA099>
2024-02-27 12:56:14 +0100

    renard

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,11 +8,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f; // Vitesse de d�placement du joueur
     [SerializeField] private float angularSpeed = 5f;
 
+    private Rigidbody rb;
+    float horizontalInput = 0f;
+    float verticalInput = 0f;
+    Vector3 rotation;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = 0f;
-        float verticalInput = 0f;
 
         // V�rifie quel joueur est en train de jouer et ajuste les entr�es en cons�quence
         if (gameObject.CompareTag("Player2"))
@@ -24,17 +33,13 @@ public class PlayerMovement : MonoBehaviour
             horizontalInput = Input.GetAxis("Horizontal");
             verticalInput = Input.GetAxis("Vertical");
         }
-
-
-        Vector3 movement = transform.position + transform.TransformDirection(Vector3.forward) * verticalInput * speed * Time.deltaTime;
-        Vector3 rotation = transform.up * horizontalInput * angularSpeed * Time.deltaTime * 10;
-
-        
-        Debug.Log(rotation);
-
-        transform.Rotate(rotation);
-        transform.position = movement;
+        rotation = transform.up * horizontalInput * angularSpeed * Time.deltaTime;
     }
+    private void FixedUpdate()
+        {
+        transform.Rotate(rotation);
+        rb.AddForce(transform.TransformDirection(Vector3.forward) * verticalInput * speed);
+        }
 }
 
 
