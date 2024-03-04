87d559395bb9b2fd57998748719e7dbcfd13babd
unknown <ycaillot@gamingcampus.local>
2024-03-04 10:48:49 +0100

    chapo couleur

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,10 @@ public class Player : MonoBehaviour
     public enum PlayerEnum { player1, player2, AI};
     [SerializeField] private PlayerEnum playerEnum;
 
+    [SerializeField] private Material redMat;
+    [SerializeField] private Material blueMat;
+
+
     private GameObject go;
 
     public PlayerEnum GetPlayerEnum()
@@ -29,36 +33,46 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log(playerEnum);
+        Transform skinPlayer = go.transform.Find("SkinPlayer");
+        Transform modelPlayer = go.transform.Find("ModelPlayer");
+        Transform chapo = go.transform.Find("Chapo");
 
-        if(playerEnum == PlayerEnum.player1)
+        Transform camera = go.transform.Find("PlayerCamera");
+        camera.tag = "MainCamera";
+
+
+        if (playerEnum == PlayerEnum.player1)
         {
             go.name = "Player1";
             go.layer = 6;
 
-            go.transform.Find("PlayerCamera").GetComponent<Camera>().cullingMask &= ~(1 << 6);
-            go.transform.Find("PlayerCamera").tag = "MainCamera";
+            camera.GetComponent<Camera>().cullingMask &= ~(1 << 6);
 
-            go.transform.Find("SkinPlayer").gameObject.layer = 6;
-            go.transform.Find("ModelPlayer").gameObject.layer = 6;
+            skinPlayer.gameObject.layer = 6;
+            modelPlayer.gameObject.layer = 6;
+            chapo.gameObject.layer = 6;
+
+            chapo.transform.Find("ChapoModel").GetComponent<SkinnedMeshRenderer>().material = blueMat;
         }
 
         else if (playerEnum == PlayerEnum.player2 || playerEnum == PlayerEnum.AI)
         {
             go.layer = 7;
 
-            go.transform.Find("PlayerCamera").GetComponent<Camera>().cullingMask &= ~(1 << 7);
+            camera.GetComponent<Camera>().cullingMask &= ~(1 << 7);
 
-            Rect tempRect = go.transform.Find("PlayerCamera").GetComponent<Camera>().rect;
+            Rect tempRect = camera.GetComponent<Camera>().rect;
             tempRect.x = 0.5f;
 
-            go.transform.Find("PlayerCamera").GetComponent<Camera>().rect = tempRect;
-            go.transform.Find("PlayerCamera").tag = "MainCamera";
+            camera.GetComponent<Camera>().rect = tempRect;
 
-            go.transform.Find("SkinPlayer").gameObject.layer = 7;
-            go.transform.Find("ModelPlayer").gameObject.layer = 7;
+            skinPlayer.gameObject.layer = 7;
+            modelPlayer.gameObject.layer = 7;
+            chapo.gameObject.layer = 7;
 
-            if(playerEnum == PlayerEnum.player2)
+            chapo.transform.Find("ChapoModel").GetComponent<SkinnedMeshRenderer>().material = redMat;
+
+            if (playerEnum == PlayerEnum.player2)
             {
                 go.name = "Player2";
             }
