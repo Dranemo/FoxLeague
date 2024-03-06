44a68ad1ccd31e905c75593464064009f8e4fa36
mguellaff <mguellaff@GCFRPA104>
2024-03-06 12:39:58 +0100

    sound

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -7,7 +7,6 @@ public class Goal : MonoBehaviour
 {
     public enum PlayerGoal { Player_1,Player_2 }
     [SerializeField] private PlayerGoal playerGoal;
-
     public void SetGoal(PlayerGoal _playerGoal)
     {
         playerGoal = _playerGoal;
